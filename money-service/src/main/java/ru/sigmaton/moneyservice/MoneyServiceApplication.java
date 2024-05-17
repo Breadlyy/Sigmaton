76158f9ab102f762cf417b41bbbc676df34e8180
shmvsky <shmvsky1337@gmail.com>
2024-05-17 20:06:52 +0300

    adds main module

---
+++ money-service/src/main/java/ru/sigmaton/moneyservice/MoneyServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.sigmaton.moneyservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoneyServiceApplication.class, args);
+    }
+
+}
--- money-service/src/test/java/ru/sigmaton/moneyservice/MoneyServiceApplicationTests.java
+++ money-service/src/test/java/ru/sigmaton/moneyservice/MoneyServiceApplicationTests.java
@@ -1,13 +1,13 @@
-package ru.sigmaton.moneyhelper;
+package ru.sigmaton.moneyservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MoneyHelperApplicationTests {
+class MoneyServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
