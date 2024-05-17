f5bc04e7292fd9832ab59ec71710b0c511a1f562
shmvsky <shmvsky1337@gmail.com>
2024-05-17 19:51:08 +0300

    initial

---
+++ src/test/java/ru/sigmaton/moneyhelper/MoneyHelperApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.sigmaton.moneyhelper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyHelperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
