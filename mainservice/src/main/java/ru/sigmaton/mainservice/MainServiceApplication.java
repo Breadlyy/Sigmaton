47aeb6d65730916d380bb33883fe4f38e0b4409a
shmvsky <shmvsky1337@gmail.com>
2024-05-17 20:46:51 +0300

    adds entry point

---
+++ mainservice/src/main/java/ru/sigmaton/mainservice/MainServiceApplication.java
@@ -0,0 +1,14 @@
+package ru.sigmaton.mainservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class MainServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainServiceApplication.class);
+    }
+
+}
