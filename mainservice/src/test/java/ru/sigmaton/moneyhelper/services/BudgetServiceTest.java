48de15bce7adecffae4f057eeeadfbdc85649caf
nikitosik-761 <ndlysenko2004@gmail.com>
2024-05-19 01:07:54 +0300

    added category test

--- mainservice/src/test/java/ru/sigmaton/moneyhelper/services/BudgetServiceTest.java
+++ mainservice/src/test/java/ru/sigmaton/moneyhelper/services/BudgetServiceTest.java
@@ -3,10 +3,8 @@ package ru.sigmaton.moneyhelper.services;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import ru.sigmaton.moneyhelper.exception.BudgetNotExists;
 import ru.sigmaton.moneyhelper.model.Account;
 import ru.sigmaton.moneyhelper.model.Budget;
 import ru.sigmaton.moneyhelper.repository.AccountRepository;
@@ -16,8 +14,8 @@ import java.util.Collections;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class BudgetServiceTest {
