c6af9646f6e7e63d64f2f1fc06d193028f5db7b0
nikitosik-761 <ndlysenko2004@gmail.com>
2024-05-18 17:14:51 +0300

    flyway fix

--- mainservice/src/main/java/ru/sigmaton/moneyhelper/model/Budget.java
+++ mainservice/src/main/java/ru/sigmaton/moneyhelper/model/Budget.java
@@ -1,12 +1,10 @@
 package ru.sigmaton.moneyhelper.model;
 
 import jakarta.persistence.*;
-import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
