ac11f098bb63b5744463a97b7a041105aabaca30
nikitosik-761 <ndlysenko2004@gmail.com>
2024-05-18 11:21:46 +0300

    cherry pich with fix bug with auth

--- mainservice/src/main/java/ru/sigmaton/moneyhelper/securityUtils/AuthenticationRequest.java
+++ mainservice/src/main/java/ru/sigmaton/moneyhelper/securityUtils/AuthenticationRequest.java
@@ -11,6 +11,6 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class AuthenticationRequest {
-    private String username;
+    private String login;
     private String password;
 }
