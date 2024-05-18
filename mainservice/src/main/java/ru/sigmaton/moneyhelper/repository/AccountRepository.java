a50362a8ee4ea0b8c989e9d9c14d29f8d92be08e
shmvsky <shmvsky1337@gmail.com>
2024-05-18 15:06:16 +0300

    adds nothing

--- mainservice/src/main/java/ru/sigmaton/moneyhelper/repository/AccountRepository.java
+++ mainservice/src/main/java/ru/sigmaton/moneyhelper/repository/AccountRepository.java
@@ -2,13 +2,13 @@ package ru.sigmaton.moneyhelper.repository;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 import ru.sigmaton.moneyhelper.model.Account;
 
 import java.util.Optional;
 
-@Repository
+
 public interface AccountRepository extends JpaRepository<Account, Long> {
 
     Optional<Account> findByLogin(String login);
+
 }
