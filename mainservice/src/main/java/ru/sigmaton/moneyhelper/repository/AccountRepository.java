0d51f7d3f11eef072033590740524057efc61bb5
nikitosik-761 <ndlysenko2004@gmail.com>
2024-05-18 11:04:19 +0300

    fixed bug with beans. Jwt is working. There is need to test it

--- mainservice/src/main/java/ru/sigmaton/moneyhelper/repository/AccountRepository.java
+++ mainservice/src/main/java/ru/sigmaton/moneyhelper/repository/AccountRepository.java
@@ -1,6 +1,6 @@
 package ru.sigmaton.moneyhelper.repository;
 
-import lombok.RequiredArgsConstructor;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.sigmaton.moneyhelper.model.Account;
