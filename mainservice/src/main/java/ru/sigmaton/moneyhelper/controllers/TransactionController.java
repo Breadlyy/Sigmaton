1ae42dcbb314b5fb1276e4c3910a473dfc9b5bad
shmvsky <shmvsky1337@gmail.com>
2024-05-18 21:57:18 +0300

    adds transaction service&controller logic

--- mainservice/src/main/java/ru/sigmaton/moneyhelper/controllers/TransactionController.java
+++ mainservice/src/main/java/ru/sigmaton/moneyhelper/controllers/TransactionController.java
@@ -1,24 +1,56 @@
 package ru.sigmaton.moneyhelper.controllers;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ru.sigmaton.moneyhelper.model.Transaction;
 import ru.sigmaton.moneyhelper.services.TransactionService;
 
+import java.security.Principal;
+import java.util.List;
+
 @RestController
-@RequestMapping("/api/v1/transaction")
-@RequiredArgsConstructor
+@RequestMapping("/api/v1/budget/categories/{categoryId}/transactions")
 public class TransactionController {
 
-//    private final TransactionService transactionService;
-//
-//    @PostMapping("/new")
-//    public void createNewTransaction(@RequestBody Transaction transaction)
-//    {
-//        transactionService.saveNewTransaction(transaction);
-//    }
+    private final TransactionService transactionService;
+
+    public TransactionController(TransactionService transactionService) {
+        this.transactionService = transactionService;
+    }
+
+    @GetMapping
+    public List<Transaction> getTransactions (
+            @PathVariable Long categoryId,
+            Principal principal)
+    {
+        return transactionService.getTransactions(categoryId, principal);
+    }
+
+    @GetMapping("/transactionId")
+    public Transaction getTransaction (
+            @PathVariable Long categoryId,
+            @PathVariable Long transactionId,
+            Principal principal)
+    {
+        return transactionService.getTransaction(categoryId, transactionId, principal);
+    }
+
+    @DeleteMapping
+    public void deleteTransaction (
+            @PathVariable Long categoryId,
+            @RequestBody Transaction transaction,
+            Principal principal)
+    {
+        transactionService.deleteTransaction(categoryId, transaction, principal);
+    }
+
+    @PostMapping
+    public Transaction createTransaction (
+            @PathVariable Long categoryId,
+            @RequestBody Transaction transaction,
+            Principal principal)
+    {
+        return transactionService.createTransaction(categoryId, transaction, principal);
+    }
 
 }
