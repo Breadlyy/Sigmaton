0aec99e9eea20e64648870e5499f507b0eb9c237
nikitosik-761 <ndlysenko2004@gmail.com>
2024-05-19 01:56:12 +0300

    fucking rebasing

--- mainservice/src/main/java/ru/sigmaton/moneyhelper/services/BudgetService.java
+++ mainservice/src/main/java/ru/sigmaton/moneyhelper/services/BudgetService.java
@@ -1,23 +1,11 @@
 package ru.sigmaton.moneyhelper.services;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-import ru.sigmaton.moneyhelper.exception.BudgetNotExists;
-import ru.sigmaton.moneyhelper.model.Budget;
 
-import java.security.Principal;
 
-@Service
-@RequiredArgsConstructor
+
+
 public class BudgetService {
 
-    private final AccountDetailsService accountService;
 
-
-    public Budget getBudget(Principal principal) {
-        var account = accountService.findByLogin(principal.getName());
-
-        return account.getBudget();
-    }
 
 }
