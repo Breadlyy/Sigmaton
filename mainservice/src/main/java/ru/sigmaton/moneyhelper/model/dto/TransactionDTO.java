0b15ac570f24b9d116b8960820d038ea4e47556a
Breadlyy <kirilllozanov012@gmail.com>
2024-05-19 11:36:53 +0300

    кафка сертификат

--- mainservice/src/main/java/ru/sigmaton/moneyhelper/model/dto/TransactionDTO.java
+++ mainservice/src/main/java/ru/sigmaton/moneyhelper/model/dto/TransactionDTO.java
@@ -1,15 +1,13 @@
 package ru.sigmaton.moneyhelper.model.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 @Getter
 @Setter
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class TransactionDTO {
